ca57e7eca0e983aa1f3245f8dc426e1648a96e53
Paulina Salem <paulinasalem@yahoo.com>
2018-04-10 22:53:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ end
   end
   def won?
     WIN_COMBINATIONS.detect do |combo|
-      position_taken?(combo[1]) && [combo[0]] == [combo[1]] && [combo[1]] == [combo[2]]
+      position_taken?(@board[combo[1]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
     end
   end
 def full?
